b4e2093c14d8137e04432efa31378ba0c8144acc
David Sanders <dsanders11@ucsbalum.com>
2023-02-22 18:11:57 -0800

    fix: play nice with npx

--- bin/markdownlint-cli-wrapper.ts
+++ bin/markdownlint-cli-wrapper.ts
@@ -4,10 +4,14 @@ import * as cp from 'node:child_process';
 import * as path from 'node:path';
 
 if (require.main === module) {
-  const command = process.platform === 'win32' ? 'markdownlint.cmd' : 'markdownlint';
   const { status } = cp.spawnSync(
-    path.resolve(__dirname, `../../node_modules/.bin/${command}`),
-    ['-r', path.resolve(__dirname, '../../markdownlint-rules'), ...process.argv.slice(2)],
+    process.execPath,
+    [
+      require.resolve('markdownlint-cli'),
+      '-r',
+      path.resolve(__dirname, '../../markdownlint-rules'),
+      ...process.argv.slice(2),
+    ],
     { stdio: 'inherit' },
   );
   if (status) process.exit(status);
