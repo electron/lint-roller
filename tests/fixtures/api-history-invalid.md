dfb7b91d158be7ac858be8e945fc5481bc56277a
piotrpdev <piotrpdev@gmail.com>
2024-06-19 00:51:03 +0100

    feat(api-history): basic tests

---
+++ tests/fixtures/api-history-invalid.md
@@ -0,0 +1,21 @@
+#### `win.setTrafficLightPosition(position)` _macOS_ _Deprecated_
+
+<!--
+```YAML history
+added: pr-url: https://github.com/electron/electron/pull/22533
+changes:
+  - pr-url: https://github.com/electron/electron/pull/26789
+    description: Made `trafficLightPosition` option work for `customButtonOnHover`.
+deprecated:
+  - pr-url: https://github.com/electron/electron/pull/37094
+    breaking-changes-header: deprecated-browserwindowsettrafficlightpositionposition
+```
+-->
+
+* `position` [Point](structures/point.md)
+
+Set a custom position for the traffic light buttons in frameless window.
+Passing `{ x: 0, y: 0 }` will reset the position to default.
+
+> **Note**
+> This function is deprecated. Use [setWindowButtonPosition](#winsetwindowbuttonpositionposition-macos) instead.
---
+++ tests/lint-roller-markdown-api-history.spec.ts
@@ -0,0 +1,104 @@
+import { spawnSync } from 'node:child_process';
+import { resolve } from 'node:path';
+
+import { describe, expect, it } from 'vitest';
+
+const FIXTURES_DIR = resolve(__dirname, 'fixtures');
+
+const stdoutRegex =
+  /Processed (\d+) API history block\(s\) in (\d+) document\(s\) with (\d+) error\(s\)./;
+
+function runLintMarkdownApiHistory(...args: string[]) {
+  return spawnSync(
+    process.execPath,
+    [resolve(__dirname, '../dist/bin/lint-markdown-api-history.js'), ...args],
+    { stdio: 'pipe', encoding: 'utf-8' },
+  );
+}
+
+describe('lint-roller-markdown-api-history', () => {
+  it('should run clean when there are no errors', () => {
+    const { status, stdout } = runLintMarkdownApiHistory(
+      '--root',
+      FIXTURES_DIR,
+      'api-history-valid.md',
+    );
+
+    expect(stdout).toMatch(stdoutRegex);
+    expect(Number(stdoutRegex.exec(stdout)?.[3])).toEqual(0); // 0 errors
+    expect(status).toEqual(0);
+  });
+
+  it('should not run clean when there are errors', () => {
+    const { status, stdout, stderr } = runLintMarkdownApiHistory(
+      '--root',
+      FIXTURES_DIR,
+      'api-history-invalid.md',
+    );
+
+    expect(stderr).toMatch(/YAMLParseError: Nested mappings are not allowed/);
+    expect(Number(stdoutRegex.exec(stdout)?.[3])).toEqual(1); // 1 error
+    expect(status).toEqual(1);
+  });
+
+  it('can ignore a glob', () => {
+    const { status, stdout } = runLintMarkdownApiHistory(
+      '--root',
+      FIXTURES_DIR,
+      '--ignore',
+      '**/api-history-invalid.md',
+      '{api-history-valid,api-history-invalid}.md',
+    );
+
+    expect(Number(stdoutRegex.exec(stdout)?.[1])).toEqual(1); // 1 block
+    expect(Number(stdoutRegex.exec(stdout)?.[2])).toEqual(1); // 1 document
+    expect(status).toEqual(0);
+  });
+
+  it('can ignore multiple globs', () => {
+    const { status, stdout } = runLintMarkdownApiHistory(
+      '--root',
+      FIXTURES_DIR,
+      '--ignore',
+      '**/api-history-valid.md',
+      '--ignore',
+      '**/api-history-invalid.md',
+      '{api-history-valid,api-history-invalid}.md',
+    );
+
+    expect(Number(stdoutRegex.exec(stdout)?.[1])).toEqual(0); // 0 blocks
+    expect(Number(stdoutRegex.exec(stdout)?.[2])).toEqual(0); // 0 documents
+    expect(status).toEqual(0);
+  });
+
+  it('can ignore from a file', () => {
+    const { status, stdout } = runLintMarkdownApiHistory(
+      '--root',
+      FIXTURES_DIR,
+      '--ignore-path',
+      resolve(FIXTURES_DIR, 'ignorepaths'),
+      '{api-history-valid,api-history-invalid}.md',
+    );
+
+    expect(Number(stdoutRegex.exec(stdout)?.[1])).toEqual(1); // 1 block
+    expect(Number(stdoutRegex.exec(stdout)?.[2])).toEqual(1); // 1 document
+    expect(status).toEqual(0);
+  });
+
+  it('should lint api history', () => {
+    const { status, stdout } = runLintMarkdownApiHistory(
+      '--root',
+      FIXTURES_DIR,
+      '{api-history-valid,api-history-invalid}.md',
+    );
+
+    expect(stdout).toMatch(stdoutRegex);
+    expect(Number(stdoutRegex.exec(stdout)?.[1])).toEqual(2); // 2 block
+    expect(Number(stdoutRegex.exec(stdout)?.[2])).toEqual(2); // 2 document
+    expect(Number(stdoutRegex.exec(stdout)?.[3])).toEqual(1); // 1 errors
+    expect(stdout).toMatchSnapshot();
+    expect(status).toEqual(1);
+  });
+
+  // TODO: Add more tests
+});
