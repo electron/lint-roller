c573d56a8affbfe07a2d43f5b14ee60316c5bdef
David Sanders <dsanders11@ucsbalum.com>
2024-11-04 15:46:17 -0800

    chore: convert custom rules to .cjs extension

---
+++ markdownlint-rules/index.cjs
@@ -0,0 +1,5 @@
+const EMD002 = require('./emd002.cjs');
+const EMD003 = require('./emd003.cjs');
+const EMD004 = require('./emd004.cjs');
+
+module.exports = [EMD002, EMD003, EMD004];
--- markdownlint-rules/index.js
+++
@@ -1,5 +0,0 @@
-const EMD002 = require('./emd002.js');
-const EMD003 = require('./emd003.js');
-const EMD004 = require('./emd004.js');
-
-module.exports = [EMD002, EMD003, EMD004];
